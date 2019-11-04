b0097209ee4c1237c0193f71fcad3ce8dc66bdf1
sdv22 <sdv22@live.ru>
2019-11-04 18:09:30 +0300

    Add vendor_load_properties2 for delayed vendor properties init.
    Change-Id: I98c9a4be6165f4b2e1b5cf5545e5bc926b10feae

--- init/vendor_init.h
+++ init/vendor_init.h
@@ -30,4 +30,6 @@ IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #ifndef __INIT_VENDOR__H__
 #define __INIT_VENDOR__H__
 extern void vendor_load_properties(void);
+extern void vendor_load_properties2(void);
 #endif /* __INIT_VENDOR__H__ */
+
